3a160c33865363714b0e9d8ed6428873c04c768e
NolikDM <mr.delete13@gmail.com>
2021-11-29 03:49:30 +0700

    axios, useEffect and custom loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
@@ -11,19 +11,36 @@ import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
 
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     // Call-back функция для передачи информации от PostForm родителю App
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
+
+    async function fetchPosts() {
+        setIsPostsLoading(true);
+        setTimeout(async () => {
+            const posts = await PostService.getAll();
+            setPosts(posts);
+            setIsPostsLoading(false);
+        }, 1000)
+    }
+
     // получаем post из дочернего элемента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
@@ -40,7 +57,10 @@ function App() {
           </MyModal>
           <hr style={{margin: '15px 0'}} />
           <PostFilter filter={filter} setFilter={setFilter}/>
-          <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов'/>
+          {isPostsLoading
+              ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+              : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов'/>
+          }
       </div>
   );
 }
