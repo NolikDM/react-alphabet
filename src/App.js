19a5a78a88e4fa316d3b6d5affe63ad77b7e71b8
NolikDM <mr.delete13@gmail.com>
2021-11-29 04:35:24 +0700

    pagination

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React, {useEffect, useMemo, useRef, useState} from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./components/utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll();
-        setPosts(posts);
+
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit));
     })
 
     useEffect(() => {
-        fetchPosts()
+        fetchPosts(limit, page)
     }, [])
 
     // Call-back функция для передачи информации от PostForm родителю App
@@ -39,6 +42,11 @@ function App() {
     // получаем post из дочернего элемента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page);
+        fetchPosts(limit, page);
     }
 
 
@@ -59,6 +67,11 @@ function App() {
               ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
               : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов'/>
           }
+          <Pagination
+              page={page}
+              changePage={changePage}
+              totalPages={totalPages}
+          />
       </div>
   );
 }
